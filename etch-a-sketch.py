12412cc5c66846a740b4bda78e5412e7b311133c
Professor Knight <profdevent@gmail.com>
2026-01-10 15:24:45 -0500

    add color feature

--- etch-a-sketch.py
+++ etch-a-sketch.py
@@ -1,7 +1,9 @@
 from turtle import *
 
 arrow = Turtle(shape="arrow")
+colors = ["red", "blue", "green", "black", "purple", "orange", "pink", "brown", "yellow"]
 mode("logo")
+arrow.penup()
 pen = False
 
 def mv_forward():
@@ -30,6 +32,9 @@ def click_pen():
 s = Screen()
 s.setup(700, 700)
 s.listen()
+turtle_color = s.textinput("What color do you want?", "Color:")
+if turtle_color in colors:
+    arrow.color(turtle_color)
 s.onkey(mv_forward, "Up")
 s.onkey(mv_backward, "Down")
 s.onkey(turn_left, "Left")
